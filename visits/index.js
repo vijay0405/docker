51e4a2421ecf9f37dc6a520b143272182763b745
vijay <vijay0405>
2019-04-20 22:17:29 +0530

    Node app with redis to count visitors to site running on  multiple containers

---
+++ visits/index.js
@@ -0,0 +1,19 @@
+const exp = require("express");
+const redis = require("redis");
+
+const app = exp();
+const client = redis.createClient();
+
+client.set("visits", 0);
+
+app.get("/", (req, res)=>{
+    client.get('visits', (err,  visits)=>{
+        res.send("Visitors Count: " + visits);
+        client.set('visits', parseInt(visits)+1);
+    })
+})
+
+app.listen(8081, ()=>{
+    console.log("listening on port 8081");
+})
+
